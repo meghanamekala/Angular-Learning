a560fa3defef2c878baeb7a423ee1ebd89e43cf3
Meghana Ravi <meghanaravi.1229@gmail.com>
2023-01-18 16:18:57 -0600

    sturctural directives

--- angular-lc101-projects-master/lesson2/exercises/src/app/candidates/candidates.component.ts
+++ angular-lc101-projects-master/lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -6,27 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
+
+  missionName = "LaunchCode Moonshot"
 
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
     {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
     {name: 'Bernie Beagle', data: {age: 4, mass: '15 kg', sidekick: 'Paul'}, image: 'assets/images/Paul.png'},
-    {name: 'Jackalope', data: {age: 45, mass: '33 kg', sidekick: 'Chris'}, image: 'assets/images/Chris.png'},
-    {name: 'Champ', data: {age: 9, mass: '36 kg', sidekick: 'Carly'}, image: 'assets/images/Carly.png'}
+    {name: 'Jackalope', data: {age: 45, mass: '33 kg', sidekick: 'Chris'}, image: '/assets/images/Chris.png'},
+    {name: 'Champ', data: {age: 9, mass: '36 kg', sidekick: 'Carly'}, image: '/assets/images/Carly.png'}
   ];
-
+  selected :any;
   crew = [];
-
+  editMissionName: boolean;
   constructor() { }
 
   ngOnInit() {
   }
-
   // Code the addToCrew function here:
-
+  addToCrew(){
+    this.crew.push(this.selected)
+  }
 
   // BONUS: Code the changeMissionName function here:
 
+  changeMissionName(name){
+    this.missionName = name;
+    this.editMissionName = false;
+  }
 
+  moveToSelected(candidate){
+    this.selected = candidate;
+
+   }
+
+   get checkToEnableSend(){
+    return this.selected
+    && this.crew.every(x=>x.name !== this.selected.name)
+    && this.crew.length <3;
+   }
 }
